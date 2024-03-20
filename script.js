076f21d75c063cb61c2766661e9f9a9998c49da7
BENARD.J Jules BENARD <BENARD.J@epid-vauban.fr>
2024-03-20 13:59:10 +0100

    Ajout de la popup2

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Sélection du formulaire
     const form = document.querySelector('form');
 
-    // Fonction pour afficher la popup de validation
-    function afficherPopupValidation() {
+    // Fonction pour afficher la popup de confirmation
+    function afficherPopupConfirmation() {
         // Création de la popup
         var popup = document.createElement("div");
-        popup.className = "popup-validation";
+        popup.className = "popup-confirmation";
         popup.innerHTML = "Votre demande a été envoyée avec succès.";
 
         // Ajout de la popup au corps du document
@@ -38,15 +38,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Gestionnaire d'événement pour la réponse de la requête
         xhr.onload = function () {
-            if (xhr.status === 200) {
-                // Le formulaire a été soumis avec succès
+            if (xhr.status === 202) {
+                // Les données ont été acceptées
                 console.log(xhr.responseText);
-                // Afficher la popup de validation
-                afficherPopupValidation();
+                // Afficher la popup de confirmation
+                afficherPopupConfirmation();
             } else {
-                // Il y a eu une erreur lors de la soumission du formulaire
-                console.error('Erreur lors de la soumission du formulaire.');
+                // Il y a eu une erreur ou la réponse n'est pas celle attendue
+                console.error('Erreur lors de la validation de la demande.');
             }
+        };
+
+        // Gestionnaire d'événement pour les erreurs de la requête
+        xhr.onerror = function () {
+            console.error('Erreur lors de la soumission du formulaire.');
         };
 
         // Conversion des données du formulaire en objet JSON
